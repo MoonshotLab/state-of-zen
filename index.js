ebc74c8d4f27d2c945e07718ea36947e506beb9a
Preston Richey <prichey@barkleyus.com>
2016-11-11 16:06:07 -0600

    Add get

--- index.js
+++ index.js
@@ -9,13 +9,8 @@ app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
   extended: true
 }));
 
-var room1 = false;
-var room2 = false;
-var status;
-
-app.post('/', function (req, res) {
-  if (!!req.body.room1) room1 = !room1;
-  if (!!req.body.room2) room2 = !room2;
+function getRoomStatus(room1, room2) {
+  var status;
 
   if (room1 && room2) {
     status = 'Both rooms are occupied.';
@@ -24,6 +19,23 @@ app.post('/', function (req, res) {
   } else {
     status = 'Room 1 is ' + (room1 ? 'occupied' : 'free') + '. Room 2 is ' + (room2 ? 'occupied' : 'free') + '.';
   }
+
+  return status;
+}
+
+var room1 = false;
+var room2 = false;
+var status = getRoomStatus(room1, room2);
+
+app.get('/', function (req, res) {
+  res.send(status);
+});
+
+app.post('/', function (req, res) {
+  if (!!req.body.room1) room1 = !room1;
+  if (!!req.body.room2) room2 = !room2;
+
+  status = getRoomStatus(room1, room2);
 
   console.log(status);
   res.send(status);
