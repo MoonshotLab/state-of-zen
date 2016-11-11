88bb57d9a8a0bbbfd5351e75100f5545fae7ff2a
Preston Richey <prichey@barkleyus.com>
2016-11-11 15:28:36 -0600

    Init

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
+  extended: true
+}));
+
+var room1 = false;
+var room2 = false;
+var status;
+app.post('/', function (req, res) {
+  if (!!req.body.room1) room1 = !room1;
+  if (!!req.body.room2) room2 = !room2;
+
+  if (room1 && room2) {
+    status = 'Both rooms are occupied.';
+  } else if (!room1 && !room2) {
+    status = 'Both rooms are free.';
+  } else {
+    status = 'Room 1 is ' + (room1 ? 'occupied' : 'free') + '. Room 2 is ' + (room2 ? 'occupied' : 'free') + '.';
+  }
+
+  console.log(status)
+  res.send(status);
+});
+
+app.listen(11235, function () {
+  console.log('Example app listening on port 11235!');
+});
