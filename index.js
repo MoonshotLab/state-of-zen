4f8f325566c4d08cbcf4ea31f6e7fdefb01d0a17
Preston Richey <prichey@barkleyus.com>
2016-11-11 15:46:49 -0600

    Port 11235 -> 3000

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+
+var port = 3000;
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
@@ -10,6 +12,7 @@ app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
 var room1 = false;
 var room2 = false;
 var status;
+
 app.post('/', function (req, res) {
   if (!!req.body.room1) room1 = !room1;
   if (!!req.body.room2) room2 = !room2;
@@ -22,10 +25,10 @@ app.post('/', function (req, res) {
     status = 'Room 1 is ' + (room1 ? 'occupied' : 'free') + '. Room 2 is ' + (room2 ? 'occupied' : 'free') + '.';
   }
 
-  console.log(status)
+  console.log(status);
   res.send(status);
 });
 
-app.listen(11235, function () {
-  console.log('Example app listening on port 11235!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
