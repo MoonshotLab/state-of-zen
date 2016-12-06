6a834e524ac4efd1c484ea5991eafa3d6f1f1800
Preston Richey <prichey@barkleyus.com>
2016-12-06 16:44:27 -0600

    Attempt to debug env issues

--- env-config.js
+++ env-config.js
@@ -1,11 +1,12 @@
 module.exports = function(){
   var config = process.env;
   try{
-    var vars = require('./env-vars.js');
+    var vars = require('./env-vars.js')();
     for(var prop in vars){
       config[prop] = vars[prop];
     }
   } catch(e){}
 
+  console.log(JSON.stringify(config));
   return config;
 };
